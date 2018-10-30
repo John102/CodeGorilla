ecf52bf1295d78f981bf24ce90e7625ed95287c6
John <j.vd.elzen1986@gmail.com>
2018-10-30 13:10:18 +0100

    Add files via upload

---
+++ includes/footer.inc.php
@@ -0,0 +1,34 @@
+<section id="sitemap">
+
+	<h3>Sitemap</h3>
+	<ul>
+		<li><a href="#">Introductie</a></li>
+		<li><a href="#">Nieuws</a></li>
+		<li><a href="#">Contact</a></li>
+		<li><a href="#">Login</a></li>
+	</ul>
+
+	
+
+</section>
+<section id="socialmedia">
+
+	<h3>Vind ons op social media</h3>
+
+	<a class="socialmedia block" target="_blank" href="#"><span class="fa fa-facebook"></span></a>
+	<a class="socialmedia block" target="_blank" href="#"><span class="fa fa-twitter"></span></a>
+	<a class="socialmedia block" target="_blank" href="#"><span class="fa fa-instagram"></span></a>
+	
+</section>
+<section id="contact">
+
+	<h3>Contactgegevens</h3>
+
+	<p><b>Adres:</b> 		Haagweg 1</p>
+	<p><b>Postcode:</b> 	4814 GA</p>
+	<p><b>Plaats:</b> 		Breda</p>
+	<p><b>Telefoon:</b> 	076-0000000</p>
+	<p><b>E-mail:</b> 		<a href="mailto:#">code@gorilla.nl</a></p>
+	
+
+</section>
---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+	<head>
+		<title>Code Gorilla</title>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="css/style.css" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+	<body>
+		<!-- NAVIGATIE -->
+		<nav>
+			<?php include('includes/navigatie.inc.php'); ?>
+		</nav>
+		<!-- HEADER -->
+		<header>
+			Header
+			<?php include('includes/header.inc.php'); ?>
+		</header>
+		<!-- CONTENT -->
+		<section id="inhoud">
+			Inhoud
+			<?php
+					// Check of pagina variable is gezet
+					// TRUE 
+					if(isset($_GET['pagina'])) {
+						$pagina = $_GET['pagina'];
+						$paginaLijst = array('');
+						// Als pagin is toegestaan
+						if(in_array($paginaLijst, $pagina)) {
+
+						// Als pagina niet is toegestaan
+						} else {
+							$pagina = '404.inc.php';
+						};
+
+					// FALSE
+					} else {
+						$pagina = 'introductie.inc.php';
+					}
+			?>
+		</section>
+		<!-- FOOTER -->
+		<footer>
+			<?php include('includes/footer.inc.php'); ?>
+		</footer>
+	</body>
+</html>
