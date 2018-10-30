c5d5a971f361ef4fbffa83acf0c80f3ff564187d
John <j.vd.elzen1986@gmail.com>
2018-10-30 22:45:06 +0100

    Update nieuws.inc.php

--- paginas/nieuws.inc.php
+++ paginas/nieuws.inc.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Nieuws</title>
-</head>
-<body>
-
 <h1>Nieuws</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa.<span id="dots">...</span><span id="more"> Fusce luctus vestibulum augue ut aliquet. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta.</span></p>
 <button onclick="myFunction()" id="myBtn">Lees meer</button>
-<script src="js/functies.js"></script>
-</body>
-</html>
+
