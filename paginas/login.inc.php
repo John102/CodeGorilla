c3c3c63dfbd7ecfdd54ee6891112e79680518af6
John <j.vd.elzen1986@gmail.com>
2018-10-30 21:25:45 +0100

    Update login.inc.php

--- paginas/login.inc.php
+++ paginas/login.inc.php
@@ -1,11 +1,19 @@
-<form action="#" method="POST">
-	<label for="naam">Naam:</label>
-    <input type="text" name="naam" placeholder="Naam" required/> 
-    <br/><br/>
-    <label for="wachtwoord">Wachtwoord:</label>
-    <input type="password" id="password" name="wachtwoord" placeholder="Wachtwoord" required/>
-    <input type="checkbox" onchange="SHPassword(this);"><span id="showhidepwd">Show</span>
-    <input type="submit" value="Aanmelden" required/>
+<form action="handlers/handler_login.php" method="POST">
+	<label for="email">E-mail:</label>
+	<input type="text" name="email" placeholder="email" required/> 
+    	
+	<br/>
+	<br/>
+	
+    	<label for="wachtwoord">Wachtwoord:</label>
+    	<input type="password" id="password" name="wachtwoord" placeholder="Wachtwoord" required/>
+
+    	<input type="checkbox" onchange="SHPassword(this);"><span id="showhidepwd">Show</span>
+    	
+	<br/>
+	<br/>
+	
+	<input type="submit" value="Aanmelden" />
   </form>
 
 <script>
