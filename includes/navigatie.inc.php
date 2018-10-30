ecd0e4c9f9445818b91de44851964166e22af8f0
John <j.vd.elzen1986@gmail.com>
2018-10-30 23:29:05 +0100

    Latest Update (dataless)

--- includes/navigatie.inc.php
+++ includes/navigatie.inc.php
@@ -7,8 +7,8 @@
 
 <ul>
   <li><a href="index.php?pagina=introductie">Introductie</a></li>
-  <li><a href="index.php?pagina=reviews">Reviews</a></li>
   <li><a href="index.php?pagina=nieuws">Nieuws</a></li>
+  <li><a href="index.php?pagina=reviews">Reviews</a></li>
   <li><a href="index.php?pagina=contact">Contact</a></li>
   
 <?php	
