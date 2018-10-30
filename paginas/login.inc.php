bdcaa5a63d2b2355f0a1aa859129971cab787c05
JoeValor <44608086+JoeValor@users.noreply.github.com>
2018-10-30 21:13:39 +0100

    Add files via upload

--- paginas/login.inc.php
+++ paginas/login.inc.php
@@ -1,9 +1,26 @@
 <form action="#" method="POST">
 	<label for="naam">Naam:</label>
-    <input type="text" required/>
+    <input type="text" name="naam" placeholder="Naam" required/> 
     <br/><br/>
     <label for="wachtwoord">Wachtwoord:</label>
-    <input type="text" required/>
-    <br/><br/>
+    <input type="password" id="password" name="wachtwoord" placeholder="Wachtwoord" required/>
+    <input type="checkbox" onchange="SHPassword(this);"><span id="showhidepwd">Show</span>
     <input type="submit" value="Aanmelden" required/>
-  <form/>
+  </form>
+
+<script>
+	function SHPassword(x)
+	{
+		var checkbox=x.checked;
+		if(checkbox)
+		{
+			document.getElementById("password"). type="text";
+			document.getElementById("showhidepwd"). textcontent="Hide"
+		}
+		else
+		{   
+			document.getElementById("password"). type="text";
+			document.getElementById("showhidepwd"). textcontent="Hide"
+		}
+	}
+</script>
