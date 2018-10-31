faaa40b93c42c46398b0955b07d96e6540923132
Siebvik <44607858+Siebvik@users.noreply.github.com>
2018-10-31 12:54:44 +0100

    Add files via upload

--- paginas/introductie.inc.php
+++ paginas/introductie.inc.php
@@ -5,12 +5,15 @@
 <link href="style1.css" rel="stylesheet" type="text/css">
 </head>
 <body> 
-<h1>Welkom op deze pagina</h1> 
+<h1 class="kleur midden" >Welkom op deze pagina</h1> 
+<p class="introductie">Onze organisatie zet zich in voor de nieuwe Nederlanders die hun talenten willen gebruiken om (verder) te integreren. We kijken er al naar uit om samen met u te bekijken waar en hoe we uw kennis en kunde in kunnen zetten. Verder kunnen mensen zich aanmelden om van u diensten gebruik te maken.</span> </p>
+<h2 class="kleur">Hoe gaat het in zijn werk?</h2>
+<h3 class="kleur">Aanbod</h3>
+	<p class="introductie">Heeft u bepaalde vaardigheden of talenten die u in uw land van herkomst heeft kunnen inzetten, maar die nu niet benut worden, dan kunt u contact met ons opnemen via het contactformulier.<br> <span class="introductie block">Nadat u zich heeft aangemeld voor een afspraak mailen of bellen we u voor een intakegesprek. Vervolgens spreken we het een en ander door met u en plaatsen we, indien er een match is, uw aanbod op onze pagina.
+<h3 class="kleur">Vraag</h3>
+Zoekt u gedreven mensen voor een dienst, dan kunt u gebruik maken van de vraag- of aanbodpagina om een passende kandidaat te vinden. U kunt en later een evaluatie schrijven.</span></p>
+<div align="center"><button id="contact" onClick="parent.location='contact.inc.php'","_self">Contact</button></div>
 
-<p>Onze organisatie zet zich in voor de nieuwe Nederlanders die hun talenten willen gebruiken om (verder) te integreren. Heeft u bepaalde vaardigheden of talenten die u in uw land van herkomst heeft kunnen inzetten, maar die nu niet benut worden, dan kunt u contact met ons opnemen via het contactformulier. We kijken er al naar uit om samen met u te bekijken waar en hoe we uw kennis en kunde in kunnen zetten. 
-</p>
-
- 	
 
 </body>
 </html>
